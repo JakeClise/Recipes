570d4e1a20a7349773252b28b94c00fe4758e992
JakeClise <jhclise@gmail.com>
2023-01-16 13:21:37 -0500

    add register validation and comparing at login

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,5 +1,9 @@
 from flask_app import app
-from flask import render_template, redirect
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+from flask import render_template, redirect, request, session, flash
+from flask_app.models.user import User
+
 
 @app.route('/')
 def home():
@@ -8,6 +12,36 @@ def home():
 @app.route('/bypass')
 def skip_to_welcome():
     return redirect('/welcome')
+
+@app.route('/register/user', methods = ["POST"])
+def register():
+    if not User.validate_user(request.form):
+        return redirect('/')
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    print(pw_hash)
+    data = {
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "email": request.form['email'],
+        "password": pw_hash
+    }
+    user_id = User.save(data)
+    session['user_id'] = user_id
+    return redirect ('/welcome')
+
+@app.route('/login/user', methods = ["POST"])
+def login():
+    data = {"email": request.form['email']}
+    user_in_db = User.get_by_email(data)
+    if not user_in_db:
+        flash("invalid Email/PW")
+        return redirect('/')
+    if not bcrypt.check_password_hash(user_in_db.password, request.form['password']):
+        flash("invalid Email/PW")
+        return redirect('/')
+    session['user_id'] = user_in_db.id
+    return redirect('/welcome')
+
 
 @app.route('/welcome')
 def welcome_user():
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,4 +1,7 @@
 from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.t_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 class User:
     def __init__(self,data):
@@ -10,11 +13,43 @@ class User:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
         users_recipes = []
+    
+    @staticmethod
+    def validate_user(user):
+        is_valid = True
+        if len(user['first_name']) < 2:
+            flash("first name must be at least 2 characters!")
+            is_valid = False
+        if len(user['last_name']) < 2:
+            flash("last name must be at least 2 characters!")
+            is_valid = False
+        if not EMAIL_REGEX.match(user['email']):
+            flash("Invalid email address!")
+            is_valid = False
+        if user['password'] != user['confpassword']:
+            flash("passwords do not match!")
+            is_valid = False
+        if len(user['password']) < 8:
+            flash("password must be at least 8 characters!")
+            is_valid = False
+        return is_valid
+
+    
 
     @classmethod
-    def register_user(query, data):
+    def save(query, data):
         query = """
             INSERT INTO users (first_name, last_name, email)
             VALUES (%(first_name)s, %(last_name)s, %(email)s);
         """
         return connectToMySQL('users_recipes').query_db(query, data)
+
+    @classmethod
+    def get_by_email(cls,data):
+        query = """
+            SELECT * FROM users WHERE email = %(email)s;
+        """
+        result = connectToMySQL('users_recipes').query_db(query, data)
+        if len(result) < 1: 
+            return False
+        return cls (result[0])
